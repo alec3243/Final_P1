5998123daf16c1a675eae3b4da06694857a9f687
alec3243 <alec3243@gmail.com>
2018-12-09 21:02:18 -0800

    Now uses processes for each player

--- Final_P1/src/player/Main.java
+++ Final_P1/src/player/Main.java
@@ -1,62 +1,62 @@
 package player;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class Main {
+	private static final int LAUNCHER_PORT = 1999;
 
 	public static void main(String[] args) throws IOException {
+
 		if (args.length == 0 || !args[0].matches("[0-9]*")) {
 			System.err
 					.println("Enter number of games to play as a command-line argument.");
 			System.exit(1);
 		}
+		DatagramSocket processReceiver = new DatagramSocket(LAUNCHER_PORT);
 		int numGames = Integer.parseInt(args[0]);
 		for (int i = 1; i <= numGames; i++) {
 			System.out.println("Game number " + i + "~~~~~");
 			try {
-				// Establish the 3 players
-				Player p1 = new Player(1);
-				Player p2 = new Player(2);
-				Player p3 = new Player(3);
+				// Initialize environment data
+				String javaHome = System.getProperty("java.home");
+				String javaBin = javaHome + File.separator + "bin"
+						+ File.separator + "java";
+				String classpath = System.getProperty("java.class.path");
+				String className = Player.class.getName();
 
-				// Get results from player1
-				boolean[] p1Wins = new boolean[2];
-				p1Wins[0] = p1.future1.get();
-				p1Wins[1] = p1.future2.get();
-
-				// Get results from player2
-				boolean[] p2Wins = new boolean[2];
-				p2Wins[0] = p2.future1.get();
-				p2Wins[1] = p2.future2.get();
-
-				// Get results from player3
-				boolean[] p3Wins = new boolean[2];
-				p3Wins[0] = p3.future1.get();
-				p3Wins[1] = p3.future2.get();
+				// Create a process for each player
+				ProcessBuilder builder1 = new ProcessBuilder(javaBin, "-cp",
+						classpath, className, "1");
+				ProcessBuilder builder2 = new ProcessBuilder(javaBin, "-cp",
+						classpath, className, "2");
+				ProcessBuilder builder3 = new ProcessBuilder(javaBin, "-cp",
+						classpath, className, "3");
+				builder1.inheritIO().start();
+				builder2.inheritIO().start();
+				builder3.inheritIO().start();
 
 				int p1Score = 0;
 				int p2Score = 0;
 				int p3Score = 0;
-
-				if (p1Wins[0] && p1Wins[1]) {
-					p1Score = 2;
-				} else if (p1Wins[0] || p1Wins[1]) {
-					p1Score = 1;
+				final int PLAYERS = 3;
+				byte[] buf;
+				DatagramPacket data;
+				for (int j = 0; j < PLAYERS; j++) {
+					buf = new byte[2];
+					data = new DatagramPacket(buf, buf.length);
+					processReceiver.receive(data);
+					if (buf[0] == 1) {
+						p1Score = buf[1];
+					} else if (buf[0] == 2) {
+						p2Score = buf[1];
+					} else if (buf[0] == 3) {
+						p3Score = buf[1];
+					}
 				}
 
-				if (p2Wins[0] && p2Wins[1]) {
-					p2Score = 2;
-				} else if (p2Wins[0] || p2Wins[1]) {
-					p2Score = 1;
-				}
-
-				if (p3Wins[0] && p3Wins[1]) {
-					p3Score = 2;
-				} else if (p3Wins[0] || p3Wins[1]) {
-					p3Score = 1;
-				}
 				if (p1Score == 1 && p2Score == 1 && p3Score == 1) {
 					p1Score = 0;
 					p2Score = 0;
@@ -65,9 +65,11 @@ public class Main {
 				System.out
 						.printf("Player 1 score - %d%nPlayer 2 score - %d%nPlayer 3 Score - %d%n",
 								p1Score, p2Score, p3Score);
-			} catch (IOException | InterruptedException | ExecutionException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+		processReceiver.disconnect();
+		processReceiver.close();
 	}
 }
--- Final_P1/src/player/Player.java
+++ Final_P1/src/player/Player.java
@@ -6,46 +6,79 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Player {
-	private static final int PLAYER1_PORT1 = 2000;
-	private static final int PLAYER1_PORT2 = 2001;
-	private static final int PLAYER2_PORT1 = 2002;
-	private static final int PLAYER2_PORT2 = 2003;
-	private static final int PLAYER3_PORT1 = 2004;
-	private static final int PLAYER3_PORT2 = 2005;
+	private static final int LAUNCHER_PORT = 1999;
+
+	static final int PLAYER1_PORT1 = 2000;
+	static final int PLAYER1_PORT2 = 2001;
+	static final int PLAYER1_OUTPORT = 2010;
+
+	static final int PLAYER2_PORT1 = 2002;
+	static final int PLAYER2_PORT2 = 2003;
+	static final int PLAYER2_OUTPORT = 2011;
+
+	static final int PLAYER3_PORT1 = 2004;
+	static final int PLAYER3_PORT2 = 2005;
+	static final int PLAYER3_OUTPORT = 2012;
+
+	static final String HOST = "localhost";
+
+	public static void main(String[] args) throws NumberFormatException,
+			UnknownHostException, IOException, InterruptedException,
+			ExecutionException {
+		new Player(Integer.parseInt(args[0]));
+	}
 
 	private Callable<Boolean> connection1;
 	private Callable<Boolean> connection2;
 	Future<Boolean> future1;
 	Future<Boolean> future2;
 
-	Player(int playerNum) throws UnknownHostException, IOException {
-
-		// connection1 initiates, connection2 waits
+	Player(int playerNum) throws UnknownHostException, IOException,
+			InterruptedException, ExecutionException {
+		DatagramSocket outputSocket = null;
+		byte[] buf = new byte[2];
 		if (playerNum == 1) {
 			connection1 = new GameConnection(PLAYER1_PORT1, PLAYER2_PORT1);
 			connection2 = new GameConnection(PLAYER1_PORT2, PLAYER3_PORT1);
+			outputSocket = new DatagramSocket(PLAYER1_OUTPORT);
+			buf[0] = 1;
 		} else if (playerNum == 2) {
 			connection1 = new GameConnection(PLAYER2_PORT1, PLAYER1_PORT1);
 			connection2 = new GameConnection(PLAYER2_PORT2, PLAYER3_PORT2);
+			outputSocket = new DatagramSocket(PLAYER2_OUTPORT);
+			buf[0] = 2;
 		} else if (playerNum == 3) {
 			connection1 = new GameConnection(PLAYER3_PORT1, PLAYER1_PORT2);
 			connection2 = new GameConnection(PLAYER3_PORT2, PLAYER2_PORT2);
+			outputSocket = new DatagramSocket(PLAYER3_OUTPORT);
+			buf[0] = 3;
 		}
 
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 		future1 = executor.submit(connection1);
 		future2 = executor.submit(connection2);
 		executor.shutdown();
+		byte score = 0;
+		if (future1.get()) {
+			score++;
+		}
+		if (future2.get()) {
+			score++;
+		}
+		buf[1] = score;
+		DatagramPacket packet = new DatagramPacket(buf, buf.length,
+				InetAddress.getByName(HOST), LAUNCHER_PORT);
+		outputSocket.send(packet);
 	}
 
 	private class GameConnection extends DatagramSocket implements
 			Callable<Boolean> {
-		private static final String HOST = "localhost";
 		private int targetPort;
 
 		GameConnection(int sourcePort, int targetPort)
@@ -100,5 +133,4 @@ public class Player {
 			return GameDecision.values()[buf[0]];
 		}
 	}
-
 }
